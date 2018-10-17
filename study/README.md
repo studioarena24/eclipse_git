7dda3d27d90546f81bd07265f2b2ea82f6ea2307
Seoyeon Ma <okyeon1@naver.com>
2018-10-17 13:34:09 +0900

    55

---
+++ study/README.md
@@ -0,0 +1,8 @@
+remote repository of git_02
+git add README.md
+git commit -m "remote repository add a README.md"
+exit
+cancel
+remote repository of git_02
+git add README.md
+git commit -m "remote repository add a README.md"
---
+++ study/test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello Git World\n");
+	printf("Tell Your world\n");
+	printf("Tell his world\n");
+	printf("Hello Git World\n");
+	printf("Tell Your world\n");
+	printf("Tell his world\n");
+	printf("Hello Git World\n");
+	printf("Tell Your world\n");
+	printf("Tell his world\n");
+}
