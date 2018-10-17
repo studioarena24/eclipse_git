9de188f7608e18663bcdcdf87c8177397bc1df6d
Seoyeon Ma <okyeon1@naver.com>
2018-10-17 13:19:47 +0900

    3nd

--- 20124981/src/helloworld.java
+++ 20124981/src/helloworld.java
@@ -3,7 +3,7 @@ public class helloworld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello, world from hotfix");
+		System.out.println("hello, world from eclipse");
 	}
 
 }
